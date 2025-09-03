e784f5572380c24d0cd073bbc9fcd01467dd4379
johno1233 <jwartonick@gmail.com>
2025-09-03 10:10:17 -0400

    Added Code: Uses shape to dictate dimensionality

--- getting-started.py
+++ getting-started.py
@@ -18,4 +18,15 @@ print(f"Ones Tensor: \n {x_ones} \n")
 x_rand = torch.rand_like(x_data, dtype=torch.float)
 print(f"Random Tensor: \n {x_rand} \n")
 
+print("---------------------- With Shape------------------")
+# Shape: tuple that determines the dimensionality of the output tensor
+shape = (2,3,)
+rand_tensor = torch.rand(shape)
+ones_tensor = torch.ones(shape)
+zeros_tensor = torch.zeros(shape)
+
+print(f"Random Tensor: \n {rand_tensor} \n")
+print(f"Ones Tensor: \n {ones_tensor} \n")
+print(f"Zeros Tensor: \n {zeros_tensor}")
+
 
