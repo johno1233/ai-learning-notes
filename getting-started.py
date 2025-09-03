604f0f069603b46c93372d3e15d1487c439e74e6
johno1233 <jwartonick@gmail.com>
2025-09-03 10:13:02 -0400

    Added Code: displays Tensor attributes

--- getting-started.py
+++ getting-started.py
@@ -29,4 +29,11 @@ print(f"Random Tensor: \n {rand_tensor} \n")
 print(f"Ones Tensor: \n {ones_tensor} \n")
 print(f"Zeros Tensor: \n {zeros_tensor}")
 
+# Attributes
+print("----------------------Attributes---------------------")
+tensor = torch.rand(3,4)
+
+print(f"Shape of tensor: {tensor.shape}")
+print(f"Datatype of tensor: {tensor.dtype}")
+print(f"Device tensor is stored on: {tensor.device}")
 
